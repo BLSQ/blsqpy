754788e8f80ce3280e039d017f32566c213ac4d2
Stéphan Mestach <smestach@bluesquare.org>
2018-12-11 13:27:58 +0100

    implements yearly, montly, quarterly split

--- blsqpy/periods.py
+++ blsqpy/periods.py
@@ -1,7 +1,10 @@
 
+import math
 from datetime import date
 from dateutil.relativedelta import relativedelta
 from dateutil.parser import parse as parseDate
+from abc import ABC, abstractmethod
+
 
 class DateRange():
     def __init__(self, start, end):
@@ -19,10 +22,22 @@ class ExtractPeriod:
         while True:
             array.append(self.dhis2_format(current_date))
             current_date = self.next_date(current_date)
-            if(current_date > date_range.last):
+            if(current_date > date_range.end):
                 break
 
         return array
+
+    @abstractmethod
+    def first_date(self, date_range):
+        pass
+
+    @abstractmethod
+    def dhis2_format(self, date_range):
+        pass
+
+    @abstractmethod
+    def next_date(self, date_range):
+        pass
 
 
 class ExtractMonthlyPeriod(ExtractPeriod):
@@ -33,29 +48,33 @@ class ExtractMonthlyPeriod(ExtractPeriod):
         return date.strftime("%Y%m")
 
     def first_date(self, date_range):
-        return date_range.first.beginning_of_month
+        return date_range.start.replace(day=1)
 
 
 class ExtractQuarterlyPeriod(ExtractPeriod):
-    def next_date(self, date):
-        return date.next_quarter
+    def next_date(self, current_date):
+        quarterStart = date(current_date.year,
+                            (current_date.month - 1) // 3 * 3 + 1, 1)
+        nextDate = quarterStart + relativedelta(months=3)
+        return nextDate
 
-    def dhis2_format(self, date):
-        return date.strftime("%Y") + "Q" + (date.month / 3.0).ceil.to_s
+    def dhis2_format(self, current_date):
+        return current_date.strftime("%Y") + "Q" + str(math.ceil((current_date.month / 3.0)))
 
     def first_date(self, date_range):
-        return date_range.first.beginning_of_quarter
+        dt = date_range.start
+        return date(dt.year, (dt.month - 1) // 3 * 3 + 1, 1)
 
 
 class ExtractYearlyPeriod(ExtractPeriod):
     def next_date(self, date):
-        return date.next_year
+        return date.replace(year=date.year + 1, month=1, day=1)
 
     def dhis2_format(self, date):
         return date.strftime("%Y")
 
     def first_date(self, date_range):
-        return date_range.first.beginning_of_year
+        return date_range.start.replace(month=1, day=1)
 
 
 CLASSES_MAPPING = {
@@ -70,9 +89,9 @@ class YearParser:
     def parse(period):
         if (len(period) != 4):
             return
-        year = period[0:3]
-        start_date = parseDate("#{year}-01-01")
-        end_date = start_date.end_of_year
+        year = int(period[0:4])
+        start_date = date(year=year, month=1, day=1)
+        end_date = start_date.replace(month=12, day=31)
 
         return DateRange(start_date, end_date)
 
@@ -83,12 +102,14 @@ class YearQuarterParser:
         if "Q" not in period:
             return
         components = period.split("Q")
-        quarter = int(components.last)
-        year = int(components.first)
-        month = (3 * (quarter - 1)) + 1
-        start_date = parseDate("#{year}-#{month}-01")
+        quarter = int(components[1])
+        year = int(components[0])
+        month_start = (3 * (quarter - 1)) + 1
+        month_end = month_start+2
+        start_date = date(year=year, month=month_start, day=1)
+        end_date = date(year=year, month=month_end, day=1)
 
-        return DateRange(start_date, start_date.end_of_quarter)
+        return DateRange(start_date, end_date)
 
 
 class YearMonthParser:
@@ -96,10 +117,10 @@ class YearMonthParser:
     def parse(period):
         if len(period) != 6:
             return
-        year = period[0:3]
-        month = int(period[4:5])
-        start_date = parseDate("#{year}-#{month}-01")
-        end_date = start_date.end_of_month
+        year = int(period[0:4])
+        month = int(period[4:6])
+        start_date = date(year=year, month=month, day=1)
+        end_date = start_date + relativedelta(day=31)
 
         return DateRange(start_date, end_date)
 
@@ -112,10 +133,15 @@ class Periods:
     @staticmethod
     def split(period, frequency):
         date_range = Periods.as_date_range(period)
-        return CLASSES_MAPPING[frequency].new(date_range)
+        mapper = CLASSES_MAPPING[frequency]
+        periods = mapper().call(date_range)
+        return periods
 
     @staticmethod
     def as_date_range(period):
         dateRange = None
         for parser in PARSERS:
             dateRange = parser.parse(period)
+            if dateRange:
+                break
+        return dateRange
--- specs/periods_spec.py
+++ specs/periods_spec.py
@@ -2,8 +2,35 @@ from mamba import description, context, it
 from expects import expect, equal
 from blsqpy.periods import Periods
 
-with description('dhis2 periods') as self:
-    with it('converts month to quarter'):
-        expect(Periods.split("201601", "quarterly")).to(["2016Q1"])
-        expect(Periods.split("201602", "quarterly")).to(["2016Q1"])
-        expect(Periods.split("201603", "quarterly")).to(["2016Q1"])
+with description("dhis2 periods") as self:
+    with it("converts month to quarter"):
+        expect(Periods.split("201601", "quarterly")).to(equal(["2016Q1"]))
+        expect(Periods.split("201602", "quarterly")).to(equal(["2016Q1"]))
+        expect(Periods.split("201603", "quarterly")).to(equal(["2016Q1"]))
+
+    with it("converts month to year"):
+        expect(Periods.split("201601", "yearly")).to(equal(
+            ["2016"]))
+
+    with it("converts quarter to months"):
+        expect(Periods.split("2016Q1", "monthly")).to(equal(
+            ["201601", "201602", "201603"]))
+        expect(Periods.split("2016Q2", "monthly")).to(
+            equal(["201604", "201605", "201606"]))
+        expect(Periods.split("2016Q3", "monthly")).to(
+            equal(["201607", "201608", "201609"]))
+        expect(Periods.split("2016Q4", "monthly")).to(
+            equal(["201610", "201611", "201612"]))
+
+    with it("converts quarter to year"):
+        expect(Periods.split("2016Q1", "yearly")).to(equal(
+            ["2016"]))
+
+    with it("converts year to quarter"):
+        expect(Periods.split("2016", "quarterly")).to(equal(
+            ["2016Q1", "2016Q2", "2016Q3", "2016Q4"]))
+
+    with it("converts year to months"):
+        expect(Periods.split("2016", "monthly")).to(equal(
+            ['201601', '201602', '201603', '201604', '201605', '201606', '201607', '201608', '201609', '201610', '201611', '201612']))
+
