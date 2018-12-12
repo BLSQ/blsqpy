b4abcea489cd48b74901a0fcdb01127f76d7e82e
mestachs <mestachs>
2018-12-12 19:47:13 +0100

    Add LICENSE and try to fix not such module

---
+++ blsqpy/__init__.py
@@ -0,0 +1,6 @@
+from . import descriptor
+from . import dot
+from . import dhis2
+from . import periods
+from . import postgres_hook
+from . import s3export_hook
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 import setuptools
 
+from setuptools import setup, find_packages
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
     name='blsqpy',
     version='0.1',
     scripts=[],
@@ -13,7 +18,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BLSQ/blsq-py",
-    packages=['blsqpy'],
+    packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
         "pandas", "boto3", "python-dotenv", "psycopg2-binary"
     ],
