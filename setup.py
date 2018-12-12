788e29e1beec75a7b6110b70f5ba5c56f2240c4d
Stéphan Mestach <smestach@bluesquare.org>
2018-12-12 12:50:40 +0100

    match package name and repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='blsq-py',
+    name='blsqpy',
     version='0.1',
     scripts=[],
     author="BLSQ",
