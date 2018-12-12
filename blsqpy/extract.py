491bebdd6dd1d8d609e78bc85b8115af57547b55
mestachs <mestachs>
2018-12-12 21:58:48 +0100

    WIP extract with specs

---
+++ blsqpy/extract.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from blsqpy.descriptor import Descriptor
+
+
+def to_data_elements(activity):
+    data_elements = []
+    for _source_code, source in Descriptor.as_items(activity):
+        for _state_code, state in Descriptor.as_items(source.states):
+            data_elements.extend(state.uids)
+    return data_elements
+
+
+def rotate_de_coc_as_columns(df):
+    df = df.rename(index=str, columns={
+        "uidorgunit": "orgunit",
+        "catcomboid": "coc",
+        "dataelementid": "de"
+    })
+
+    df["period"] = np.where(df.monthly, df.monthly, df.quarterly)
+    df = df.drop(["uidlevel3", "uidlevel2", "enddate",
+                  "dataelementname", "catcomboname", "created",
+                  "quarterly", "monthly"], axis=1)
+
+    df["de.coc"] = df.de.str.cat(df.coc, '.')
+
+    df.drop(['de', 'coc'], axis=1, inplace=True)
+
+    df = df.set_index(['period', 'orgunit', 'de.coc'],
+                      drop=True).unstack('de.coc')
+    return df["value"]
---
+++ blsqpy/mapping.py
@@ -0,0 +1,51 @@
+import pandas as pd
+
+from blsqpy.descriptor import Descriptor
+
+
+def to_expressions(activity):
+    expressions = {}
+
+    for state_code, state in Descriptor.as_items(activity.states):
+        if len(state.uids) > 1:
+            state_expressions = []
+            for idx in range(len(state.uids)):
+                state_expressions.append(state_code+"_"+str(idx+1))
+            expressions[state_code] = state_expressions
+    return expressions
+
+
+def to_mappings(activity, activity_code):
+    mappings = {}
+    prefix = ''
+    if activity_code:
+        prefix = activity_code+"_"
+    for state_code, state in Descriptor.as_items(activity.states):
+        if len(state.uids) == 1:
+            mappings[state.uids[0]] = prefix+state_code
+        else:
+            for idx, uid in enumerate(state.uids):
+                mappings[uid] = prefix+state_code+"_"+str(idx+1)
+    return mappings
+
+
+def map_from_activity(df, activity):
+
+    mappings = to_mappings(activity, None)
+
+    df = df.rename(index=str, columns=mappings)
+
+    # make sure we consider these a numerics, or + eval will concatenate instead of summing
+    for column, de in mappings.items():
+        df[str(de)] = pd.to_numeric(df[str(de)],
+                                    errors='ignore', downcast='float')
+
+    # TODO generate eval from config
+    eval_expressions = to_expressions(activity)
+
+    for column, columns_to_sum in eval_expressions.items():
+        eval_expression = column + ' = '+(" + ".join(columns_to_sum))
+        df.eval(eval_expression, inplace=True, engine="python")
+        df.drop(columns_to_sum, axis=1, inplace=True)
+
+    return df.reset_index()
