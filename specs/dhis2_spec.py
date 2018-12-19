bf4dd89e9e267959211e9f5914190e7df432ce70
Stéphan Mestach <smestach@bluesquare.org>
2018-12-19 10:57:01 +0100

    Move reusable setup in MockHook

--- specs/dhis2_spec.py
+++ specs/dhis2_spec.py
@@ -19,13 +19,18 @@ init_sql_to_df = {
 class MockHook:
     def __init__(self, sqls_to_dfs):
         self.sqls_to_dfs = sqls_to_dfs
-        #print(self.sqls_to_dfs)
+        # print(self.sqls_to_dfs)
 
     def get_pandas_df(self, sql):
         csv = self.sqls_to_dfs[sql]
         #print(sql, csv)
 
         return pd.read_csv("./specs/dhis2/sql_"+csv+".csv")
+
+    @staticmethod
+    def with_extra_sqls(sqls):
+        all_sqls = {**sqls, **init_sql_to_df}
+        return MockHook(all_sqls)
 
 
 with description('Dhis2') as self:
@@ -34,12 +39,10 @@ with description('Dhis2') as self:
 
     with it("data_elements_structure"):
         sqls = {
-           "SELECT\ndataelement.uid AS de_uid,\ntrim(regexp_replace( dataelement.name ,'\\s+', ' ', 'g')) AS  de_name  , \ntrim(regexp_replace( dataelement.shortname ,'\\s+', ' ', 'g')) AS  de_shortname  , \ncategoryoptioncombo.uid AS coc_uid,\ntrim(regexp_replace( categoryoptioncombo.name ,'\\s+', ' ', 'g')) AS  coc_name  , \ncategoryoptioncombo.code AS coc_code,\ncategorycombo.uid AS cc_uid,\ntrim(regexp_replace( categorycombo.name ,'\\s+', ' ', 'g')) AS  cc_name  , \ncategorycombo.code AS cc_code\nFROM dataelement\nJOIN categorycombo ON categorycombo.categorycomboid = dataelement.categorycomboid\nJOIN categorycombos_optioncombos ON categorycombos_optioncombos.categorycomboid = categorycombo.categorycomboid\nJOIN categoryoptioncombo ON categoryoptioncombo.categoryoptioncomboid = categorycombos_optioncombos.categoryoptioncomboid":
+            "SELECT\ndataelement.uid AS de_uid,\ntrim(regexp_replace( dataelement.name ,'\\s+', ' ', 'g')) AS  de_name  , \ntrim(regexp_replace( dataelement.shortname ,'\\s+', ' ', 'g')) AS  de_shortname  , \ncategoryoptioncombo.uid AS coc_uid,\ntrim(regexp_replace( categoryoptioncombo.name ,'\\s+', ' ', 'g')) AS  coc_name  , \ncategoryoptioncombo.code AS coc_code,\ncategorycombo.uid AS cc_uid,\ntrim(regexp_replace( categorycombo.name ,'\\s+', ' ', 'g')) AS  cc_name  , \ncategorycombo.code AS cc_code\nFROM dataelement\nJOIN categorycombo ON categorycombo.categorycomboid = dataelement.categorycomboid\nJOIN categorycombos_optioncombos ON categorycombos_optioncombos.categorycomboid = categorycombo.categorycomboid\nJOIN categoryoptioncombo ON categoryoptioncombo.categoryoptioncomboid = categorycombos_optioncombos.categoryoptioncomboid":
             "data_elements_structure"
         }
-        all_sqls = {**sqls, **init_sql_to_df}
-
-        dhis2 = Dhis2(MockHook(all_sqls))
+        dhis2 = Dhis2(MockHook.with_extra_sqls(sqls))
 
         des = dhis2.data_elements_structure()
 
