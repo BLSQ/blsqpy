f99e7b7bd504135301a744c892ac6796bfebb3c5
FVB-BLSQ <45073988+FVB-BLSQ@users.noreply.github.com>
2018-12-21 09:51:30 +0100

    Smaller changes to match the new input label data

--- specs/reconcile_spec.py
+++ specs/reconcile_spec.py
@@ -7,16 +7,12 @@ import blsqpy.data_process as dp
 from mamba import description, it
 
 
-config = json.loads(open("./specs/config/sample_2_sources.json",
-                         encoding='utf-8').read())
-input_data = pd.read_csv('./specs/reconcile/input.csv', sep=';')
-
 with description("reconciliation") as self:
     with it("sequential reconciliation with prefered source"):
         config = json.loads(open("./specs/config/sample_2_sources.json", encoding='utf-8').read())
         input_data = pd.read_csv('./specs/reconcile/input.csv', sep=';')
         expected_data = pd.read_csv('./specs/reconcile/expected.csv', sep=';')
-        serie = dp.measured_serie(mapped_data, config, 'pills', 'delivered', 'moh')
+        serie = dp.measured_serie(input_data, config, 'pills', 'delivered', 'moh')
         reconciled_serie = serie.reconcile_series()
         pd.testing.assert_frame_equal(
             reconciled_serie.data,
