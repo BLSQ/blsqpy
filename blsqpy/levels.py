33a2fb5f1ebcb42ed13fdd27195d7934249e2687
Stéphan Mestach <smestach@bluesquare.org>
2019-01-02 14:30:59 +0100

    Initial implementation for pyramid vs dataset orgunits vs dataset coverage

--- blsqpy/levels.py
+++ blsqpy/levels.py
@@ -14,15 +14,29 @@ class Levels:
         return levels - 1
 
     @staticmethod
-    def add_uid_levels_columns_from_path_column(df):
+    def add_uid_levels_columns_from_path_column(df, start = 2, end_offset= -1):
       max_level = Levels.max_level(df)
 
       def to_level(select_level, x):
             return Levels.to_level_uid(x[0], select_level)
 
-      for level in range(2, max_level - 1):
+      for level in range(start, max_level + end_offset):
             df["uidlevel"+str(level)] = df[['path']
                                            ].apply(partial(to_level, level), axis=1)
 
       df.drop(["path"], axis=1, inplace=True)
       return df
+
+    @staticmethod
+    def add_uid_levels_columns_from_path_column_full(df):
+        max_level = Levels.max_level(df)
+
+        def to_level(select_level, x):
+                return Levels.to_level_uid(x[0], select_level)
+
+        for level in range(1, max_level +1):
+                df["uidlevel"+str(level)] = df[['path']
+                                            ].apply(partial(to_level, level), axis=1)
+
+        df.drop(["path"], axis=1, inplace=True)
+        return df
