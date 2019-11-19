bd614c44890e238f4e46cfd87ad5b9df78fb7a4a
Julie Desombre <jdesombre@bluesquarehub.com>
2019-11-19 15:00:15 +0100

    Move geometry function from test script to production code

--- geodata.py
+++ geodata.py
@@ -3,54 +3,17 @@ import geopandas
 import matplotlib.pyplot as plt
 import json
 from blsqpy.dhis2_client import Dhis2Client
-from shapely.geometry import LineString, Polygon, MultiPolygon, Point, shape
-
-
-def as_geometry(coordinates):
-    x = json.loads(coordinates)
-    if coordinates.startswith("[[[["):
-        geojson = {"type": "MultiPolygon", "coordinates": x}
-        return shape(geojson)
-    if coordinates.startswith("[[["):
-        geojson = {"type": "Polygon", "coordinates": x}
-        return shape(geojson)
-    if coordinates.startswith("[["):
-        return LineString(x)
-    if coordinates.startswith("["):
-        return Point(float(x[0]), float(x[1]))
-
-
-def geometrify(orgunits):
-    for orgunit in orgunits:
-        if "coordinates" in orgunit:
-            orgunit["geometry"] = as_geometry(orgunit["coordinates"])
-
-
+from blsqpy.geometry import geometrify
 client = Dhis2Client("https://admin:district@play.dhis2.org/2.30")
 
-orgunits = client.get("organisationUnits",
-                      {
-                          "fields": "id,name,featureType,coordinates,level",
-                          "filter": [
-                              "featureType:!eq:POINT",
-                              "level:eq:3"
-                          ]
-                      }
-                      )["organisationUnits"]
+gdf=client.get_geodataframe()
 
-geometrify(orgunits)
-
-df = pd.DataFrame(orgunits)
-
-gdf = geopandas.GeoDataFrame(df)
 world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
 ax = world[world.continent == 'Africa'].plot(
     color='white', edgecolor='black')
 gdf.plot(ax=ax, color='red')
 plt.show()
 
-
-df = pd.DataFrame(orgunits)
 
 orgunits = client.get("organisationUnits",
                       {
